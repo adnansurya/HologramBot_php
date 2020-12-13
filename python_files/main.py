75ae747c6fad2ccf6df7945e5c65d62161b93b92
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2020-12-13 21:22:56 +0800

    sendpost to hologrambot

--- python_files/main.py
+++ python_files/main.py
@@ -1,7 +1,23 @@
 import serial
+import urllib.request as req
+import urllib.parse as par
+
+def sendPost(card_id):
+    apiurl = 'https://betaku.000webhostapp.com/hologramBot/absen.php'
+    data = {
+            'card' : card_id
+           
+        }
+    data = bytes(par.urlencode(data).encode())
+    handler = req.urlopen(apiurl,data)
+    print(handler.read().decode('utf-8'))
 
 ser = serial.Serial('COM5', 9600)
 
 while True:
     line = ser.readline()
-    print(line.strip().decode( "utf-8" ))
+    clean_line = line.strip().decode( "utf-8" )
+    if(line != ''):
+        
+        print('ID : ' + str(clean_line))
+        sendPost(clean_line)
