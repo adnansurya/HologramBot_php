32d6afa98e92df0374c1f03579d7bb05ef13c3e3
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2020-12-13 21:05:06 +0800

    change microcontroller to arduino

--- arduino_files/readRFID_ESP8266/readRFID_ESP8266.ino
+++ arduino_files/readRFID_ESP8266/readRFID_ESP8266.ino
@@ -11,8 +11,8 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define SS_PIN D8
-#define RST_PIN D3
+#define SS_PIN 10
+#define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
  
 void setup() 
