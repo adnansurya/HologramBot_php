0888a28eb1dbdd18491f1a0eff96fb1e977138c9
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2020-12-17 20:30:00 +0800

    http POST ESP32

---
+++ arduino_files/httppost_ESP32/httppost_ESP32.ino
@@ -0,0 +1,62 @@
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "HOLOGRAM2";
+const char* password = "untukapa?";
+
+//Your Domain name with URL path or IP address with path
+const char* serverName = "http://betaku.000webhostapp.com/hologramBot/absen.php";
+
+
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP()); 
+
+    if(WiFi.status()== WL_CONNECTED){
+      HTTPClient http;
+      
+      // Your Domain name with URL path or IP address with path
+      http.begin(serverName);
+
+      // Specify content-type header
+      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+      // Data to send with HTTP POST
+      String httpRequestData = "card=04-34-1F-CA-1B-29-80";           
+      // Send HTTP POST request
+      int httpResponseCode = http.POST(httpRequestData);
+      
+      // If you need an HTTP request with a content type: application/json, use the following:
+      //http.addHeader("Content-Type", "application/json");
+      //int httpResponseCode = http.POST("{\"api_key\":\"tPmAT5Ab3j7F9\",\"sensor\":\"BME280\",\"value1\":\"24.25\",\"value2\":\"49.54\",\"value3\":\"1005.14\"}");
+
+      // If you need an HTTP request with a content type: text/plain
+      //http.addHeader("Content-Type", "text/plain");
+      //int httpResponseCode = http.POST("Hello, World!");
+     
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+        
+      // Free resources
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+
+}
+
+void loop() {
+  //Send an HTTP POST request every 10 minutes
+  
+}
