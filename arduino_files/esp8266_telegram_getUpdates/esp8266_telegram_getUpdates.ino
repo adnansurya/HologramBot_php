d86a5fe37d9fb0aa9e309878d126809e2472d67d
Adnan <muh.adnansuryaazis@gmail.com>
2021-05-29 17:21:17 +0800

    last process on ASUS

---
+++ arduino_files/esp8266_telegram_getUpdates/esp8266_telegram_getUpdates.ino
@@ -0,0 +1,116 @@
+
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h> 
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+
+/* Set these to your desired credentials. */
+const char *ssid = "HOLOGRAM2";  //ENTER YOUR WIFI SETTINGS
+const char *password = "untukapa?";
+
+//Link to read data from https://jsonplaceholder.typicode.com/comments?postId=7
+//Web/Server address to read/write from 
+const char *host = "https://api.telegram.org";
+const int httpsPort = 443;  //HTTPS= 443 and HTTP = 80
+
+//SHA1 finger print of certificate use web browser to view and copy
+const char fingerprint[] PROGMEM = "F2 AD 29 9C 34 48 DD 8D F4 CF 52 32 F6 57 33 68 2E 81 C1 90";
+//=======================================================================
+//                    Power on setup
+//=======================================================================
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_OFF);        //Prevents reconnection issue (taking too long to connect)
+  delay(1000);
+  WiFi.mode(WIFI_STA);        //Only Station No AP, This line hides the viewing of ESP as wifi hotspot
+  
+  WiFi.begin(ssid, password);     //Connect to your WiFi router
+  Serial.println("");
+
+  Serial.print("Connecting");
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  //If connection successful show IP address in serial monitor
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
+
+  
+}
+
+//=======================================================================
+//                    Main Program Loop
+//=======================================================================
+void loop() {
+  httpGetReq();
+  
+}
+
+void httpGetReq(){
+WiFiClientSecure httpsClient;    //Declare object of class WiFiClient
+
+  Serial.println(host);
+
+//  Serial.printf("Using fingerprint '%s'\n", fingerprint);
+//  httpsClient.setFingerprint(fingerprint);
+  httpsClient.setTimeout(15000); // 15 Seconds
+  delay(1000);
+  
+  Serial.print("HTTPS Connecting");
+  int r=0; //retry counter
+  while((!httpsClient.connect(host, httpsPort)) && (r < 30)){
+      delay(100);
+      Serial.print(".");
+      r++;
+  }
+  if(r==30) {
+    Serial.println("Connection failed");
+  }
+  else {
+    Serial.println("Connected to web");
+  }
+  
+  String getData, Link;
+  
+
+  //GET Data
+  Link = "/bot1564078066:AAHpbk7ryIumTCBIhMsxz4tvXF62KEEEDLk/getUpdates?offset=-1";
+
+  Serial.print("requesting URL: ");
+  Serial.println(host+Link);
+
+  httpsClient.print(String("GET ") + Link + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +               
+               "Connection: close\r\n\r\n");
+
+  Serial.println("request sent");
+                  
+  while (httpsClient.connected()) {
+    String line = httpsClient.readStringUntil('\n');
+    if (line == "\r") {
+      Serial.println("headers received");
+      break;
+    }
+  }
+
+  Serial.println("reply was:");
+  Serial.println("==========");
+  String line;
+  while(httpsClient.available()){        
+    line = httpsClient.readStringUntil('\n');  //Read Line by Line
+    Serial.println(line); //Print response
+  }
+  Serial.println("==========");
+  Serial.println("closing connection");
+    
+  delay(10000);  //GET Data at every 2 seconds  
+  
+}
