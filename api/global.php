38afc146a3da86365d3c2d031a6ed8cd9dae291a
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2021-10-22 14:12:16 +0800

    send Image with caption

--- api/global.php
+++ api/global.php
@@ -43,7 +43,36 @@
         $output = curl_exec($ch);                    
         return $output;
     }
+    function sendImageCaption($chatId, $filepic, $caption, $tokenAPI){
+        $bot_url    = "https://api.telegram.org/bot".$tokenAPI."/";
+        $url        = $bot_url . "sendPhoto?chat_id=" . $chatId ;
+        
+        $post_fields = array('chat_id'   => $chatId,
+            'photo'     => new CURLFile(realpath($filepic)),
+            'caption'   => $caption
+        );
+        
+        $ch = curl_init(); 
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            "Content-Type:multipart/form-data"
+        ));
+        curl_setopt($ch, CURLOPT_URL, $url); 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields); 
+        $output = curl_exec($ch);                    
+        return $output;
+    }
+
 
     $date = new DateTime("now", new DateTimeZone('Asia/Makassar') );
     $waktu = $date->format('d-m-Y H:i:s');
+
+    function selisihWaktu($waktunya){
+        global $date;
+        $d1 = strtotime($waktunya);
+        $d2 = strtotime( $date->format('Y-m-d H:i:s'));
+        $totalSecondsDiff = abs($d1-$d2);
+        return $totalSecondsDiff;
+    }
+   
 ?>
